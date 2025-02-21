462f82779d852f9fd7ad7942ca463ae88bc25be2
Hiep Tran <hieptt149@gmail.com>
2025-02-21 15:36:58 +0700

    oauth2 with opaque token

--- spring-security-demo/src/main/java/com/hieptt149/spring/security/demo/config/ProjectSecurityConfig.java
+++ spring-security-demo/src/main/java/com/hieptt149/spring/security/demo/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.hieptt149.spring.security.demo.config;
 
 import com.hieptt149.spring.security.demo.exceptionhandling.CustomAccessDeniedHandler;
 import com.hieptt149.spring.security.demo.filter.CsrfCookieFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -20,6 +21,15 @@ import java.util.Collections;
 @Configuration
 @Profile("!prod")
 public class ProjectSecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.opaquetoken.introspection-uri}")
+    String introspectionUri;
+
+    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-id}")
+    String clientId;
+
+    @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-secret}")
+    String clientSecret;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -55,9 +65,15 @@ public class ProjectSecurityConfig {
                                 .requestMatchers("/notices", "/contact", "/error", "/register").permitAll()
                 )
                 .exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()))
-                .oauth2ResourceServer(rsc -> rsc.jwt(
+                /*.oauth2ResourceServer(rsc -> rsc.jwt(
                         jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)
-                ));
+                ))*/
+                .oauth2ResourceServer(rsc ->
+                        rsc.opaqueToken(opaqueTokenConfigurer -> opaqueTokenConfigurer.authenticationConverter(new KeyCloakOpaqueRoleConverter())
+                                .introspectionUri(introspectionUri)
+                                .introspectionClientCredentials(clientId, clientSecret)
+                        )
+                );
         return httpSecurity.build();
     }
 }
