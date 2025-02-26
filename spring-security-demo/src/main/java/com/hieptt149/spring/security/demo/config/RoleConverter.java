09636c24d1a17f32b29f9a46d1018bc68fb4b174
Hiep Tran <hieptt149@gmail.com>
2025-02-26 22:54:40 +0700

    customize access token for client credentials grant type flow

--- spring-security-demo/src/main/java/com/hieptt149/spring/security/demo/config/RoleConverter.java
+++ spring-security-demo/src/main/java/com/hieptt149/spring/security/demo/config/RoleConverter.java
@@ -8,14 +8,13 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
-        List<String> roles = (List<String>) source.getClaims().get("scope");
+        List<String> roles = (List<String>) source.getClaims().get("roles");
 
         if (roles == null || roles.isEmpty()) {
             return new ArrayList<>();
